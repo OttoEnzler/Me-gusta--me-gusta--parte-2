26d5964af4b126ca20adcddd11e241fc52c8807b
OttoEnzler <enzler.otto@gmail.com>
2024-04-21 05:19:27 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function like1(){
+    var likes = document.querySelector("#num1");
+    likes.innerHTML = parseInt(likes.innerHTML) + 1;
+}
+function like2(){
+    var likes = document.querySelector("#num2");
+    likes.innerHTML = parseInt(likes.innerHTML) + 1;
+}
+function like3(){
+    var likes = document.querySelector("#num3");
+    likes.innerHTML = parseInt(likes.innerHTML) + 1;
+}
